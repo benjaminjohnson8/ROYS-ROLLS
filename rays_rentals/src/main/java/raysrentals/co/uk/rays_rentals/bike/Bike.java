b75031715477cdbed3e3f6933e6491645f48ba19
ROakley <ROakley@September2-51.myfuturesbright.local>
2015-10-08 10:58:33 +0100

    changed bike model

---
+++ rays_rentals/src/main/java/raysrentals/co/uk/rays_rentals/bike/Bike.java
@@ -0,0 +1,93 @@
+package raysrentals.co.uk.rays_rentals.bike;
+
+import java.time.LocalDate;
+import java.util.Currency;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Type;
+
+
+@Entity()
+@Table(name = "bike_record")
+public class Bike implements java.io.Serializable  {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 825004720238685773L;
+	
+	@Id
+	@GeneratedValue
+	private Long id;
+	
+	@Column
+	private String model;
+	
+	@Column
+	private String manufacturer;
+	
+	@Column
+	@Type(type="org.jadira.usertype.dateandtime.threeten.PersistentLocalDate")
+	private LocalDate purchaseDate;
+	
+	@Column
+	private Currency price;
+	
+	@Column(name = "is_unavailable")
+	private Boolean unavailable;
+	//TODO the rest
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public String getManufacturer() {
+		return manufacturer;
+	}
+
+	public void setManufacturer(String manufacturer) {
+		this.manufacturer = manufacturer;
+	}
+
+	public LocalDate getPurchaseDate() {
+		return purchaseDate;
+	}
+
+	public void setPurchaseDate(LocalDate purchaseDate) {
+		this.purchaseDate = purchaseDate;
+	}
+
+	public Currency getPrice() {
+		return price;
+	}
+
+	public void setPrice(Currency price) {
+		this.price = price;
+	}
+
+	public void setUnavailable(Boolean unavailable) {
+		this.unavailable = unavailable;
+	}
+	
+	public boolean isUnavaiable()
+	{
+		return unavailable;
+	}
+}
