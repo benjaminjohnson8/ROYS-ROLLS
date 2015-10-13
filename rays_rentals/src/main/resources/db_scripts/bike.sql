4919655ccf78044c2efad60c573369197825c3c0
ROakley <ROakley@September2-51.myfuturesbright.local>
2015-10-13 16:14:27 +0100

    more bike stuff

--- rays_rentals/src/main/resources/db_scripts/bike.sql
+++ rays_rentals/src/main/resources/db_scripts/bike.sql
@@ -1,8 +1,8 @@
-INSERT INTO bike_record(id, manufacturer, model)
-VALUES(109, 'Apollo', 'X781'),
-(110, 'Apollo', 'X781'),
-(111, 'Apollo', 'X781'),
-(112, 'Apollo', 'X781'),
-(113, 'Apollo', 'X781'),
-(114, 'Apollo', 'X781');
+INSERT INTO bike_record(id, manufacturer, model, price, unavailable, purchaseDate)
+VALUES(109, 'Apollo', 'X781','28', true, sysDate()),
+(110, 'Apollo', 'X781','28', true, sysDate() ),
+(111, 'Apollo', 'X781','28',  true, sysDate()),
+(112, 'Apollo', 'X781','28', true, sysDate()),
+(113, 'Apollo', 'X781','28', true, sysDate()),
+(114, 'Apollo', 'X781','28', true, sysDate());
 
